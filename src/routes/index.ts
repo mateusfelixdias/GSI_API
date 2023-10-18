19bbf94415ed901997f41f125d9944a808634f63
Jefte <jefte199@gmail.com>
2023-10-17 22:16:46 -0300

    feat: finish imoveis controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import { getWelcomeMessage } from '../Controllers/wellcome';
+import { ImoveisController } from '../Controllers/ImoveisController';
 
 export const routes = Router();
 
 // Rota para teste 
 routes.get('/', getWelcomeMessage.index)
+routes.get('/imoveis/getAll', ImoveisController.index)
+routes.get('/imoveis/getOne/:id', ImoveisController.show)
+routes.post('/imoveis/create', ImoveisController.store)
+routes.put('/imoveis/update/:id', ImoveisController.update)
+routes.delete('/imoveis/delete/:id', ImoveisController.update)
