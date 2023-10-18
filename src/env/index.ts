f4c548788a6f8ede7e70541004602ae7984e2390
Jefte <jefte199@gmail.com>
2023-10-17 22:12:56 -0300

    feat: config zod

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,17 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config'
+import { z } from 'zod'
 
-export const env = {
-  PORT: process.env.PORT,
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Error: variables', _env.error.format())
+
+  throw new Error('Invalid variable')
 }
+
+export const env = _env.data
