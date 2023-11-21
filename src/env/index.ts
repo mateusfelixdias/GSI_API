f3f200db832dfc3dea42df6e28284192ec5b5ff7
mateusfelixdias <felixmateus618@gmail.com>
2023-11-21 16:57:32 -0300

    refactor: leaving variables optional

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,13 +4,10 @@ import { z } from 'zod';
 const envSchema = z.object({
   DATABASE_URL: z.string(),
   PORT: z.number().optional().default(3333),
-  NODE_ENV: z
-    .enum(['development', 'test', 'production'])
-    .optional()
-    .default('development'),
 });
 
 const _env = envSchema.safeParse(process.env);
+console.log(process.env);
 if (!_env.success) throw new Error('Invalid variable');
 
 export const env = _env.data;
